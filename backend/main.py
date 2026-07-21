67d3809582bb259f2a5bcd53676bc63d67bea916
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-21 22:44:50 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,35 @@
- 
+from fastapi import FastAPI
+
+from database import engine
+from models import Base
+
+
+app = FastAPI(
+    title="STMA",
+    description="Smart Telegram Message Archive",
+    version="1.0"
+)
+
+
+# Создание таблиц базы данных
+Base.metadata.create_all(
+    bind=engine
+)
+
+
+@app.get("/")
+def home():
+
+    return {
+        "app": "STMA",
+        "status": "online",
+        "version": "1.0"
+    }
+
+
+@app.get("/health")
+def health():
+
+    return {
+        "ok": True
+    }
